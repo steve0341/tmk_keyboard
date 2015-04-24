9d9e2512a8ae8385358a490412b90dded0e24d5a
Steve Vang <svang@denniskirk.com>
2015-04-24 14:26:27 -0500

    Increased mouse speed

--- common/mousekey.h
+++ common/mousekey.h
@@ -39,10 +39,10 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define MOUSEKEY_INTERVAL 30
 #endif
 #ifndef MOUSEKEY_MAX_SPEED
-#define MOUSEKEY_MAX_SPEED 10
+#define MOUSEKEY_MAX_SPEED 14
 #endif
 #ifndef MOUSEKEY_TIME_TO_MAX
-#define MOUSEKEY_TIME_TO_MAX 20
+#define MOUSEKEY_TIME_TO_MAX 25
 #endif
 #ifndef MOUSEKEY_WHEEL_MAX_SPEED
 #define MOUSEKEY_WHEEL_MAX_SPEED 8
